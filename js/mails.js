0009b435958cbf27597f5fed8e169f8029168aa2
Emerdinger <82613672+Emerdinger@users.noreply.github.com>
2021-05-18 12:24:42 -0500

    subi la pagina principal

---
+++ js/mails.js
@@ -0,0 +1,37 @@
+(function () {
+    emailjs.init("user_M9tyz9Vrx5NYAJzjste3P");
+})();
+const vue = new Vue({
+    el: '#app',
+    data() {
+        return {
+            from_name: '',
+            from_email: '',
+            message: '',
+            from_phone: '',
+        }
+    },
+    methods: {
+        enviar() {
+            let data = {
+                from_name: this.from_name,
+                from_email: this.from_email,
+                message: this.message,
+                from_phone: this.from_phone,
+            };
+            console.log(data);
+            emailjs.send("service_f7yak5n", "template_gk5hgjx", data)
+                .then(function (response) {
+                    if (response.text === 'OK') {
+                        alert('El correo se ha enviado de forma exitosa');
+                        location.reload();
+                        emailjs.send("service_f7yak5n", "template_xeutcap", data)
+                    }
+                    console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+                }, function (err) {
+                    alert('Ocurrió un problema al enviar el correo');
+                    console.log("FAILED. error=", err);
+                });
+        }
+    }
+});
