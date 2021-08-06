c00474ff39fe001f43d5feba068e81cf91e850bf
Emerdinger <servicesemer@gmail.com>
2021-08-05 23:31:05 -0500

    Hoja de vida

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -25,7 +25,8 @@ const Inicio = () => {
                     <h5>Crece y personaliza tu emprendimiento</h5>
                     <h1>Soy Emerson Niño</h1>
                     <p>Haz llegar tu negocio a mas personas con una página web a la medida de tus requerimientos</p>
-                    <a href="/contacto">Descargar CV</a>
+                    <a href="https://drive.google.com/file/d/1sRJByQ0_vuWkmyiG4wKfP4GOzzKaA7PD/view"
+                       download="HojaDeVidaEmerson" target="_blank">Mi hoja de vida</a>
                 </div>
                 <div className="back">
                     <img src={Webdev} alt="Web-dev"/>
