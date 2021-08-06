6836cfd79572e44584d05424f6bd1cf1e4f49624
Emerdinger <servicesemer@gmail.com>
2021-08-05 23:42:12 -0500

    routes fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {
-    BrowserRouter as Router, Switch, Route, Link
+    HashRouter as Router, Switch, Route, Link
 } from "react-router-dom";
 import "./index.css"
 import Logo from "./imagenes/logo.png"
